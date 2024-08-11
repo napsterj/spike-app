a495e5f72277817017520b27e6bd9a1359147dbf
napsterJ <softwareprofessional1095@gmail.com>
2024-08-11 20:19:44 +0100

    added validations, and method for creating new user in the form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [],
+  standalone: true,  
+  imports: [CommonModule, ReactiveFormsModule, JsonPipe],
+  providers:[FormBuilder],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+ 
+  signUpForm:any
+  
+  constructor(private fb:FormBuilder){ }
+
+  ngOnInit(): void {
+      this.signUpForm = this.fb.group({
+        firstName:['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],        
+        middleName:[''],
+        lastName : ['',Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
+        email: ['', Validators.compose([Validators.required,Validators.email])]
+      })
+  }
+
+  createNewUser() {
+    console.log(this.signUpForm)    
+  }
+
 
 }
